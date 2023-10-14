1a008ddfccafb5d6db85fb7a878478ae234b78fb
bkallaus <benkallaus@gmail.com>
2023-10-14 10:50:25 -0500

    Updates

---
+++ precache-manifest.6b0e7367d83a22b124cc4aa2359c0265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d38d075b4229bfd0a196eafad77b19b",
+    "url": "/bkallaus.github.io/index.html"
+  },
+  {
+    "revision": "8fed7b89c7fe24afc2d1",
+    "url": "/bkallaus.github.io/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "75c836336af61536e657",
+    "url": "/bkallaus.github.io/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "8fed7b89c7fe24afc2d1",
+    "url": "/bkallaus.github.io/static/js/2.8adf60db.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bkallaus.github.io/static/js/2.8adf60db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c836336af61536e657",
+    "url": "/bkallaus.github.io/static/js/main.b09bc2f5.chunk.js"
+  },
+  {
+    "revision": "c8c8e405f352563bfd03",
+    "url": "/bkallaus.github.io/static/js/runtime-main.bff51add.js"
+  }
+]);
